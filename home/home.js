2ff22acd6dbe4dd90869fa7e36c0415f55fd2393
Nour <noursaleem@arrowitjo.com>
2025-05-31 23:09:42 +0300

    fix home & project

---
+++ home/home.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const projectToggle = document.getElementById('projectToggle');
+    const projectList = document.getElementById('project_list');
+    const PmoToggle=document.getElementById('PmoToggle');
+    const pmo_list=document.getElementById('pmo_list');
+    const Property_list=document.getElementById('Property_list');
+    const PropertyToggle=document.getElementById('PropertyToggle');
+    const CustomerToggle=document.getElementById('CustomerToggle');
+    const customer_list=document.getElementById('customer_list');
+    const development_list=document.getElementById('development_list');
+    const developmentToggle=document.getElementById('developmentToggle');
+    const markitingToggle=document.getElementById('markitingToggle');
+    const markiting_list=document.getElementById('markiting_list');
+    const Purchases_list=document.getElementById('Purchases_list');
+    const pourchaseToggle=document.getElementById('pourchaseToggle');
+    const reportToggle=document.getElementById('reportToggle');
+    const report_list=document.getElementById('report_list');
+
+    projectToggle.addEventListener('click', function() {
+     if (projectList.style.display === 'flex') {
+            projectList.style.display = 'none'; // Hide if currently visible
+        } else {
+            projectList.style.display = 'flex'; // Show if currently hidden
+        }
+        
+    });
+
+    PmoToggle.addEventListener('click',function(){
+         if (pmo_list.style.display === 'flex') {
+            pmo_list.style.display = 'none'; // Hide if currently visible
+        } else {
+            pmo_list.style.display = 'flex'; // Show if currently hidden
+        }
+    });
+
+
+    PropertyToggle.addEventListener('click',function(){
+         if (Property_list.style.display === 'flex') {
+            Property_list.style.display = 'none'; // Hide if currently visible
+        } else {
+            Property_list.style.display = 'flex'; // Show if currently hidden
+        }
+    });
+     
+    CustomerToggle.addEventListener('click',function(){
+         if (customer_list.style.display === 'flex') {
+            customer_list.style.display = 'none'; // Hide if currently visible
+        } else {
+            customer_list.style.display = 'flex'; // Show if currently hidden
+        }
+    });
+
+    developmentToggle.addEventListener('click',function(){
+         if (development_list.style.display === 'flex') {
+            development_list.style.display = 'none'; // Hide if currently visible
+        } else {
+            development_list.style.display = 'flex'; // Show if currently hidden
+        }
+    });
+
+    markitingToggle.addEventListener('click',function(){
+         if (markiting_list.style.display === 'flex') {
+            markiting_list.style.display = 'none'; // Hide if currently visible
+        } else {
+            markiting_list.style.display = 'flex'; // Show if currently hidden
+        }
+    });
+
+    pourchaseToggle.addEventListener('click',function(){
+         if (Purchases_list.style.display === 'flex') {
+            Purchases_list.style.display = 'none'; // Hide if currently visible
+        } else {
+            Purchases_list.style.display = 'flex'; // Show if currently hidden
+        }
+    });
+
+    reportToggle.addEventListener('click',function(){
+         if (report_list.style.display === 'flex') {
+            report_list.style.display = 'none'; // Hide if currently visible
+        } else {
+            report_list.style.display = 'flex'; // Show if currently hidden
+        }
+    });
+});
+
+    
